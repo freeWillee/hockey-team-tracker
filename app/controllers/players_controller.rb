0764287c7167718f6fa215786cd7063d6fba513e
freeWillee <will.i.lee@outlook.com>
2018-09-21 16:28:47 -0600

    moved profile-slug views to teamsController from PlayersController

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,14 +1,5 @@
 class PlayersController < ApplicationController
 
-    get '/team/:team_name/:profile_slug' do
-        @staff = Staff.find_by_slug(params[:profile_slug])
-        @player = Player.find_by_slug(params[:profile_slug])
-        
-        if @player
-            erb :"players/show"
-        else
-            erb :"staff/show"
-        end
-    end
+    
 
 end
