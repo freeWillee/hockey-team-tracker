e68024b097bb572705ba715e90c9ee5dc5dcbea5
freeWillee <will.i.lee@outlook.com>
2018-09-26 15:01:19 -0600

    Completed edit player page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,13 +6,46 @@ class ApplicationController < Sinatra::Base
         set :session_secret, "mypassword"
     end
 
+    # HOMEPAGE ROUTE
     get '/' do
         erb :index
     end
 
     # EDIT PLAYER ==> WHY CAN'T I MOVE THIS CODE UNDER MY PLAYERS_CONTROLLER.RB??
     patch '/team/:team_slug/player/:player_slug' do
-        binding.pry
+
+        @team = Team.find_by_name(session[:team_name])
+        @player = Player.find_by_slug(params[:player_slug])
+        @editing_player_name = params[:player][:name]
+        @salary_range_selected = Salary.find_by_id(params[:player][:salary_id].to_i)
+
+        if player_exists?(@editing_player_name) && @editing_player_name != @player.name
+            #RAISE MESSAGE ERROR THAT PLAYER ALREADY EXISTS
+            existing_player = Player.find_by_name(@editing_player_name)
+
+            redirect to "/team/#{params[:team_slug]}/player/#{existing_player.slug}"
+        else
+            #Change name if not empty            
+            if !@editing_player_name.empty?
+                @player.name = @editing_player_name
+            end
+            
+            #Clear player positions & update with selected
+            @player.positions.clear
+            @player.save
+            params[:positions].each do |position|
+                @player.positions << Position.find_by_id(position.to_i)
+            end
+
+            #Update salary range if necessary
+            @player.salary = @salary_range_selected if @player.salary != @salary_range_selected
+
+            @player.save
+
+            #redirect user to player profile after creating
+            redirect to "/team/#{@player.team.slug}/player/#{@player.slug}"
+            
+        end
     end
 
     # DELETE PLAYER ==> WHY CAN'T I MOVE THIS CODE UNDER MY PLAYERS_CONTROLLER.RB??
@@ -23,6 +56,7 @@ class ApplicationController < Sinatra::Base
         erb :"players/delete"
     end
 
+    #HELPER METHODS
     helpers do
         def login(team_name, password)
             team = Team.find_by_name(team_name)
