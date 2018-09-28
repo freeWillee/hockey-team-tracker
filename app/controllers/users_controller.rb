c894c9b28701c53ec390f50b25070c76ffd969ed
freeWillee <will.i.lee@outlook.com>
2018-09-28 00:50:07 -0600

    Working through user login for users (readonly)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,30 @@
 class UsersController < ApplicationController
+    
+    #VISIT CREATE-NEW-ADMIN PAGE
     get '/user/new-admin' do
         
         erb :"/users/new_super"
     end    
 
+    #CREATE-NEW-ADMIN AND LOGIN
     post '/new-admin' do
         @user = User.new(params[:user])
         @user.super_user = 1
-        session[:username] = @user.username
+        
+        binding.pry
         @user.save
+
+        session[:username] = @user.username
         
         redirect to "/admin"
     end
 
+    #LOGIN AS ADMIN
     get '/admin' do
         if logged_in?
             @user = current_user
-            @all_users = User.all.detect{|user| user.super_user == 0}
-            
+            @all_users = User.all.map{|user| user.super_user == 0}
+            binding.pry
             erb :"/users/show_super"
         else
             @message = "You have not logged in.  Please log in below"
@@ -25,12 +32,27 @@ class UsersController < ApplicationController
         end
     end
 
+    #VISIT CREATE-NEW-USER PAGE
+    get '/user/new-user' do
+        @user = current_user
+        
+        erb :"/users/new_user"
+    end
 
+    #CREATE USER (READ-ONLY)
     post '/new-user' do
         @user = User.new(params[:user])
-        @user.super_user = 0
-        @user.save
+        if @user.valid?
+            @user.super_user = 0
+            @user.team = current_user.team
+            binding.pry
+            @user.save
 
-        erb :"/user/show_super"
+            redirect to "/admin"
+        else
+            @user.valid?
+
+            erb :"users/new_user"
+        end
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,17 @@
 class User < ActiveRecord::Base
     has_secure_password
     belongs_to :team
+    validates :username, :email, presence: true
+    validates :username, uniqueness: {case_sensitive: false}
+
+    def slug
+        name = self.name
+        slugged = name.downcase.gsub(" ", '-')
+        slugged
+    end
+
+    def self.find_by_slug(slug)
+        name = slug.split('-').join(" ")
+        self.all.detect{|i| i.name.downcase == name}
+    end
 end
